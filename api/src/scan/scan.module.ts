fe6a289c91948b6d3c02f807b487a8931e868a84
rturmanidze <128627579+rturmanidze@users.noreply.github.com>
2026-06-01 10:57:20 +0400

    Delete api directory

--- api/src/scan/scan.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ScanController } from './scan.controller';
-import { ScanService } from './scan.service';
-
-@Module({
-  controllers: [ScanController],
-  providers: [ScanService],
-})
-export class ScanModule {}
